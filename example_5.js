0dfc27995a3cfe55d7e90c9b6f3ab4205b4a9bcb
orald <orald@vbi.vt.edu>
2013-03-26 14:56:02 -0400

    backbone example 5 modified

--- example_5.js
+++ example_5.js
@@ -6,8 +6,8 @@
   
   var Item = Backbone.Model.extend({
     defaults: {
-      part1: 'hello',
-      part2: 'world'
+	  interface_id: 0,
+      text: 'Hello World'
     }
   });
   
@@ -19,19 +19,20 @@
     tagName: 'li', // name of tag to be created        
     // `ItemView`s now respond to two clickable actions for each `Item`: swap and delete.
     events: { 
-      'click span.swap':  'swap',
+      'click span.edit':  'edit',
+      'click span.done':  'done',
       'click span.delete': 'remove'
     },    
     // `initialize()` now binds model change/removal to the corresponding handlers below.
     initialize: function(){
-      _.bindAll(this, 'render', 'unrender', 'swap', 'remove'); // every function that uses 'this' as the current object should be in here
+      _.bindAll(this, 'render', 'unrender', 'edit', 'done', 'remove'); // every function that uses 'this' as the current object should be in here
 
       this.model.bind('change', this.render);
       this.model.bind('remove', this.unrender);
     },
     // `render()` now includes two extra `span`s corresponding to the actions swap and delete.
     render: function(){
-      $(this.el).html('<span style="color:black;">'+this.model.get('part1')+' '+this.model.get('part2')+'</span> &nbsp; &nbsp; <span class="swap" style="font-family:sans-serif; color:blue; cursor:pointer;">[swap]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
+      $(this.el).html('<span id="'+this.model.get('interface_id')+'" style="color:black;">'+this.model.get('text')+'</span> &nbsp; &nbsp; <span class="edit" style="font-family:sans-serif; color:blue; cursor:pointer;">[edit]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
       return this; // for chainable calls, like .render().el
     },
     // `unrender()`: Makes Model remove itself from the DOM.
@@ -39,13 +40,12 @@
       $(this.el).remove();
     },
     // `swap()` will interchange an `Item`'s attributes. When the `.set()` model function is called, the event `change` will be triggered.
-    swap: function(){
-      var swapped = {
-        part1: this.model.get('part2'), 
-        part2: this.model.get('part1')
-      };
-      this.model.set(swapped);
+    edit: function(){
+      $(this.el).html('<span style="color:black;"><input id= "input_'+this.model.get('interface_id')+'" value="'+this.model.get('text')+'"/></span> &nbsp; &nbsp; <span class="done" style="font-family:sans-serif; color:blue; cursor:pointer;">[done]</span> <span class="delete" style="cursor:pointer; color:red; font-family:sans-serif;">[delete]</span>');
     },
+	done: function(){
+      this.model.set({text:$("#input_"+this.model.get('interface_id')).val()});
+	},
     // `remove()`: We use the method `destroy()` to remove a model from its collection. Normally this would also delete the record from its persistent storage, but we have overridden that (see above).
     remove: function(){
       this.model.destroy();
@@ -79,7 +79,8 @@
       this.counter++;
       var item = new Item();
       item.set({
-        part2: item.get('part2') + this.counter // modify item defaults
+        interface_id: this.counter,
+		text: item.get('text') + this.counter // modify item defaults
       });
       this.collection.add(item);
     },
